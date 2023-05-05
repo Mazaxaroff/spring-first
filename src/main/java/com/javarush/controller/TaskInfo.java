eb6cb217ae29dcef20c5227ffcaedc34d475b173
Mazaxaroff <mzxrov@mail.ru>
2023-05-05 20:42:08 +0300

    create Service, controller and maket

---
+++ src/main/java/com/javarush/controller/TaskInfo.java
@@ -0,0 +1,13 @@
+package com.javarush.controller;
+
+import com.javarush.domain.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskInfo {
+    private String description;
+    private Status status;
+
+}
